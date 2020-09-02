70c10887e627b10c007f514220edd3f6f554f68a
yuler <joyful4yu@gmail.com>
2020-09-02 22:54:00 +0800

    Add simple way to saved transcripts to `output/*.txt`

--- main.go
+++ main.go
@@ -1,23 +1,55 @@
+// refs: http://go-colly.org/docs/examples/coursera_courses/
+
 package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"regexp"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
+	// host := "https://frontendmasters.com"
 	c := colly.NewCollector(
 		colly.AllowedDomains("frontendmasters.com"),
 	)
 
-	c.OnHTML(".CourseToc a[href]", func (e *colly.HTMLElement) {
+	// Get Course links
+	c.OnHTML(".CourseToc a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		e.Request.Visit(link)
+	})
+
+	// Get Lesson Transcript
+	c.OnHTML(".LessonTranscript", func(e *colly.HTMLElement) {
+		title := e.ChildText(".LessonTranscriptTitle")
+		title = regexp.MustCompile("\"(.*?)\"").FindStringSubmatch(title)[1]
+		transcripts := e.ChildTexts(".s-wrap p:not(:first-child)")
+		fmt.Println("====")
+		fmt.Printf("Title: %s \n", title)
+		fmt.Println("Transcripts:")
+		f, err := os.Create("output/" + title + ".txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		for i := 0; i < len(transcripts); i++ {
+			fmt.Println(transcripts[i])
+			f.WriteString(transcripts[i] + "\n")
+		}
+		fmt.Println("====")
 	})
 
 	c.OnRequest(func(r *colly.Request) {
-      fmt.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
 	})
 
 	c.Visit("https://frontendmasters.com/courses/deep-javascript-v3/")
